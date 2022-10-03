9e05ac8a89219f2eb05d45029f58c04693abffcd
Diego Calciolari Gordiano <diego.gordiano@acad.espm.br>
2022-10-03 15:12:39 -0300

    inserção do script sql

--- Projeto/app.ts
+++ Projeto/app.ts
@@ -1,3 +1,16 @@
 ﻿import app = require("teem");
 
-app.run();
+app.run({
+	// Configurações de acesso ao banco de dados.
+	// Mais informações: https://www.npmjs.com/package/mysql2#using-connection-pools
+	sqlConfig: {
+		connectionLimit: 30,
+		waitForConnections: true,
+		charset: "utf8mb4",
+		host: "localhost",
+		port: 3306,
+		user: "root",
+		password: "root",
+		database: "jogopda"
+	}
+});
