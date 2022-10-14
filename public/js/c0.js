583758da9c26f1980dbc8023f8563efd95548e14
ZOU YI LAI <zouyilai@gmail.com>
2022-10-14 16:57:29 -0300

    Unificando as pastas dos dois projetos
    😊

---
+++ public/js/c0.js
@@ -0,0 +1,111 @@
+"use strict";
+
+// Cada competência é um array de telas
+
+competencias[0] = [
+
+	{ // tela 0
+		urlImagem : "/public/img/nave.jpeg",
+		titulo: "DESAFIO 2: [Combinar Teorias]",
+		descricao: `
+			<p>
+				A Dra. Emma Schultz começa uma pequena aula introduzindo sobre o planeta.
+			</p>
+			<p>
+				DRA. EMMA SCHULTZ: "Como sabemos, a teoria da evolução de Darwin diz que os seres com a mutação mais propícia para a sobrevivência em um ambiente passam pela seleção natural e acabam se tornando a espécie escolhida em um habitat. O T0I700d é um planeta semelhante à Terra, com a presença de água e diversos seres vivos. Porém há diversas diferenças: a vegetação local pode acabar mudando constantemente de forma e além disso alguns seres são capazes de carregar um peso muito acima do usual. O ar aqui não é tóxico aos humanos, no entanto, é bastante rarefeito causando dificuldade de respirar. Por isso, podemos sentir cansaço em breves caminhadas. Usem os tubos de oxigênio caso sinta falta de ar. Ainda há muito o que não sabemos sobre o planeta, muito o que há para se descobrir. Foi descoberto recentemente um novo elemento, denominado Agnentânio, presente no solo planeta e, consequentemente, em sua vegetação, esse elemento possui propriedades químicas únicas. Foi criada então uma nova teoria: a de que esse elemento é capaz de não somente diminuir a necessidade de oxigênio dos seres vivos, mas também fornecer uma espécie de nutrição extremamente rica. Recentemente, por exemplo, uma espécie de roedor semelhante a um porquinho da Índia foi encontrado por pesquisadores, no entanto a espécie possuía algumas diferenças: cada ser possuía um total de seis patas e tinha o tamanho de um gato. Muito intrigada uma das estagiárias resolveu fazer um carinho sem antes passar pelos protocolos de segurança e testagem da Biolife. O resultado foi que o contato gerou uma irritabilidade na jovem, que teve o corpo todo tomado por vermelhidão e muita coceira".
+			</p>
+			<p>
+				QUESTAO- Sabendo do acontecimento e levando em conta a teoria da evolução e a teoria sobre o Agnentânio, o que pode-se afirmar acerca das teorias sobre a combinação de teorias?
+			</p>
+		`,
+		alternativas: [
+			{
+				descricao: " É correto afirmar que a teoria da evolução agiu para que o ser fosse selecionado de tal forma. Desse modo, ela é a única teoria válida para a explicação sobre a existência desse ser.",
+				valor: 0,
+				marcada: false,
+			},
+			{
+				descricao: " A teoria da evolução e a teoria do Agnentânio são duas teorias distintas, mas que podem ser combinadas para explicar diversos aspectos acerca da natureza do planeta. Assim como é possível fazer com teorias interdisciplinares.",
+				valor: 0,
+				marcada: false,
+			},
+			{
+				descricao: " É possível que a teoria sobre o Agnentânio seja aplicável a esse caso, mesmo que em conjunto com a teoria da evolução.",
+				valor: 0,
+				marcada: false,
+			},
+			{
+				descricao: " É possível tentar utilizadar ambas as teorias para se buscar por explicações para diversos fenônemos do planeta, desde como são os animais até a como é disposta a vegetação. Uma vez que as teorias são interdisciplinares e combináveis.",
+				valor: 0,
+				marcada: false,
+			},
+			{
+				descricao: " É estritamente necessário estudar bem o fenômeno para enquadrá-lo em apenas uma das teorias existentes, visto que o fato de que ele se encaixa em uma teoria, anula a possibilidade dele se encaixar na outra.",
+				valor: 0,
+				marcada: false,
+			},
+			{
+				descricao: " É possível que a teoria da evolução explique alguns aspectos sobre o animal, enquanto a teoria sobre o Agnentânio explique outro, de modo que combinando as duas pode ser que se chegue a uma explicação sobre o porquê do animal ser desse jeito.",
+				valor: 0,
+				marcada: false,
+			},
+			{
+				descricao: " As duas teorias podem coexistir, no entanto combiná-las para explicar tal fenômeno é impossível.",
+				valor: 0,
+				marcada: false,
+			},
+			{
+				descricao: " É possível que o fato do animal possuir um tamanho grande comparando com o porquinho da Índia terrestre se deva a uma combinação de tanto a evolução quanto o Agnentânio.",
+				valor: 0,
+				marcada: false,
+			},
+			{
+				descricao: " A evolução foi uma realidade para as espécies, assim como a existência do Agnentânio no solo local. Desse modo, as pesquisas não só sobre o porquinho, mas também sobre diferentes espécies locais podem utilizar e testar ambas as teorias, sem que uma exclua a outra. ",
+				valor: 0,
+				marcada: false,
+			},
+			{
+				descricao: " Os acontecimentos anteriores não possuem relação alguma com o atual, visto que cada caso é um caso diferente, não podendo se aplicar em outras situações.",
+				valor: 0,
+				marcada: false,
+			}, 
+			{
+				descricao: " Embora ambas as teorias tenham embasamento e possam afetar campos científicos em comum, elas agem de forma distinta, de modo que tentar combiná-las seria um erro.",
+				valor: 0,
+				marcada: false,
+			}
+		]
+	}, 
+
+	{ // tela 1
+		urlImagem: "...",
+		titulo: "AUTONOMIA",
+		descricao: "Fase 1"
+	},
+
+	{ // tela 2
+		urlImagem: "...",
+		descricao: "descricao 1"
+	},
+
+	{ // tela 3 - essa questão está muito esquisito...
+		urlImagem: "...",
+		titulo: "Titulo2",
+		descricao: "Descricao2",
+		alternativas: [
+			{
+				descricao: "2: alt1",
+				valor: 0, // Rubrica? Métrica de avaliação?...?
+				marcada: false,
+			},
+			{
+				descricao: "2:alt2",
+				valor: 0, // Rubrica? Métrica de avaliação?...?
+				marcada: false,
+			}
+		]
+	},
+
+	
+	// ...
+];
---
+++ routes/index.ts
@@ -0,0 +1,47 @@
+﻿import app = require("teem");
+
+class IndexRoute {
+
+	public async index(req: app.Request, res: app.Response){
+		res.render("index/index", {
+			layout: "layout-vazio"
+		});
+	}
+
+	@app.http.post()
+	@app.route.formData()
+	public async cadastrarResposta(req: app.Request, res: app.Response){
+		// Os dados enviados via POST ficam dentro de req.body
+		let registros: any[] = req.body;
+
+		// É sempre muito importante validar os dados do lado do servidor,
+		// mesmo que eles tenham sido validados do lado do cliente!!!
+		if (!registros || !registros.length) {
+			res.status(400);
+			res.json("Dados inválidos");
+			return;
+		}
+
+		await app.sql.connect(async (sql) => {
+
+			// Todas os comandos SQL devem ser executados aqui dentro do app.sql.connect().
+			await sql.beginTransaction();
+
+			await sql.query("INSET INTO usuario (idusuario, dt) VALUES (?, now())");
+
+			const idusuario = await sql.scalar("SELECT last_insert_id()");
+
+			for (let i = 0; i < registros.length; i++) {
+				const registro = registros[i];
+				// As interrogações serão substituídas pelos valores passados ao final, na ordem passada.
+				await sql.query("INSERT INTO registro (competencia, pergunta, resposta, idusuario) VALUES (?, ?, ?, ?)", [registro.competencia, registro.pergunta, registro.resposta, idusuario]);
+			}
+
+			await sql.commit();
+		});
+
+		res.json(true);
+	}
+}
+
+export = IndexRoute;
