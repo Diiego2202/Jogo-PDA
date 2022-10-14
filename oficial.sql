6178a3239695c0bbd8ae3013acb16a1121c7eb98
ZOU YI LAI <zouyilai@gmail.com>
2022-10-14 14:39:16 -0300

    script v2

---
+++ oficial.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS jogopda DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_0900_ai_ci;
+USE jogopda;
+
+CREATE TABLE IF NOT EXISTS usuario(
+  idusuario INT NOT NULL AUTO_INCREMENT,
+  dt INT NOT NULL,
+  PRIMARY KEY (idusuario)
+);
+
+CREATE TABLE IF NOT EXISTS registro (
+  idregistro BIGINT(64) NOT NULL AUTO_INCREMENT,
+  competencia INT NOT NULL,
+  pergunta INT NOT NULL,
+  resposta INT NOT NULL,
+  idusuario BIGINT(64) NOT NULL,
+  PRIMARY KEY (idregistro),
+  CONSTRAINT registro_usuario
+    FOREIGN KEY (idusuario)
+    REFERENCES usuario (idusuario)
+);
